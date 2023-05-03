a54493f5852d8fda673620738045145ae014972e
Puggi99 <127298032+Puggi99@users.noreply.github.com>
2023-05-03 10:22:38 +0200

    ddd

--- data-service.js
+++
@@ -1,8 +0,0 @@
-export class DataService{
-
-    static BASE_URL = 'https://catfact.ninja/breeds'
-
-    static getCatPage(){
-        return fetch(this.BASE_URL).then(resp=>resp.json())
-    }
-}
--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 import './style.css'
-import { DataService } from './dog';
+import { DataService } from './services/data-service';
+import confetti from 'canvas-confetti'
 
 
 console.log('ciao')
 
 
-getCatPage().then(data=>{
+DataService.getFacts().then(data=>{
   console.log(data)
+  displayData(data)
+  fireConfetti()
 })
+
+function displayData(data){
+  const list = document.getElementById('facts-list');
+  for (const factObj of data) {
+    list.innerHTML += `<li>${factObj.fact}</li>`    
+  }
+}
+
+function fireConfetti(){
+  confetti({
+    particleCount: 10000,
+    spread: 150,
+    origin: { y: 0.6 }
+  });
+}
